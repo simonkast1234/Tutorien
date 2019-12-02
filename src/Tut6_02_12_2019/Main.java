cd911a7cf0d16d5b04a8f561f019bfc78d0dd104
SK-Laptop <simonkast1234@gmail.com>
2019-12-02 17:43:20 +0100

    02.12.

--- src/Tut6_02_12_2019/Main.java
+++ src/Tut6_02_12_2019/Main.java
@@ -1,5 +1,7 @@
 package Tut6_02_12_2019;
 
+import Prog1Tools.IOTools;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -8,9 +10,10 @@ import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader()); // Main.class.getResource("words.txt")
+    private static final int MAXIMUM_ERRORS = 10;
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("/home/sk/IdeaProjects/Tutorien/src/Tut6_02_12_2019/words2.txt")); // Main.class.getResource("words.txt")
         String ln;
         List<String> wordsList = new ArrayList<>();
         while ((ln = reader.readLine()) != null) {
@@ -18,11 +21,56 @@ public class Main {
         }
         String[] words = wordsList.toArray(new String[0]);
 
-        String word = selectRandom(words);
-        boolean[] guessed = new boolean[word.length()];
 
-        System.out.println(word);
+        char[] word = selectRandom(words).toCharArray();
+        boolean[] guessed = new boolean[word.length];
+        int errors = 0;
 
+        while(!isTrue(guessed)) {
+
+            // Wort ausgeben
+            for (int i = 0; i < word.length; i++) {
+                if(guessed[i]) {
+                    System.out.print(word[i] + " ");
+                } else {
+                    System.out.print("_ ");
+                }
+            }
+            System.out.println("\n");
+
+            // Buchstaben einlesen
+            char c = IOTools.readChar("Buchstaben eingeben: ");
+            boolean attemptSuccessful = false;
+            for (int i = 0; i < word.length; i++) {
+                if(word[i] == c) {
+                    guessed[i] = true;
+                    attemptSuccessful = true;
+                }
+            }
+
+            if(attemptSuccessful) {
+                System.out.println("Richtig geraten");
+            } else {
+                System.out.println("Falsch geraten!");
+                errors++;
+                System.out.println("Übrige Versuche: " + (MAXIMUM_ERRORS - errors));
+            }
+
+            if(errors >= MAXIMUM_ERRORS) {
+                System.out.println("Verloren!" +"\n" + "Das Wort war " + new String(word));
+                return;
+            }
+        } // while
+
+    }
+
+    private static boolean isTrue(boolean[] guessed) { // unschön mit static zu arbeiten ...
+        for (int i = 0; i < guessed.length; i++) {
+            if(!guessed[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static String selectRandom(String[] words){
